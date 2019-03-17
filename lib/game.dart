32bb2a393c1585f1d9d7c61b60552bbb5bc8a09d
Niklas <nitohu@gmail.com>
2019-03-17 12:41:58 +0100

    [ADD] win detection (game logic)

--- lib/game.dart
+++ lib/game.dart
@@ -14,21 +14,70 @@ class _TicTacToeState extends State<TicTacToe> {
 
   Map<int, String> buttons = {};
 
+  List<int> player0Fields = List<int>();
+  List<int> player1Fields = List<int>();
+
+  String _displayText;
+
+  final List< List<int> > winFields = [
+    [0,4,8], [2,4,6],           // diagonal
+    [0,3,6], [1,4,7], [2,5,8],  // columns
+    [0,1,2], [3,4,5], [6,7,8]   //  rows
+  ];
+  final String player0Char = "X";
+  final String player1Char = "O";
+
   _TicTacToeState() {
     for(int i = 0; i < 9; i++) {
       buttons[i] = "";
     }
   }
 
+  void _resetGame() {
+    setState(() {
+      for(int i = 0; i < 9; i++) {
+        buttons[i] = "";
+      }
+      
+      _activePlayer = 0;
+      player0Fields = [];
+      player1Fields = [];
+    });
+  }
+
+  void _detectWins() {
+
+    for(List<int> wins in winFields) {
+      if(player0Fields.contains(wins[0]) && player0Fields.contains(wins[1]) && player0Fields.contains(wins[2])) {
+        setState(() {
+          _displayText = "Player 1 has won";
+          buttons[4] = "Reset";
+        });
+      } else if(player1Fields.contains(wins[0]) && player1Fields.contains(wins[1]) && player1Fields.contains(wins[2])) {
+        setState(() {
+          _displayText = "Player 2 has won";
+          buttons[4] = "Reset";
+        });
+      }
+    }
+
+  }
+
   void _buttonPressed(int id) {
-    if(buttons[id].isEmpty) {
+    if(buttons[4] == "Reset" && id == 4) {
+
+      _resetGame();
+
+    } else if(buttons[id].isEmpty && buttons[4] != "Reset") {
       setState(() {
         if(_activePlayer == 0) {
-          buttons[id] = "X";
+          buttons[id] = player0Char;
           _activePlayer = 1;
+          player0Fields.add(id);
         } else {
-          buttons[id] = "O";
+          buttons[id] = player1Char;
           _activePlayer = 0;
+          player1Fields.add(id);
         }
       });
     }
@@ -85,7 +134,7 @@ class _TicTacToeState extends State<TicTacToe> {
           ],
         ),
         SizedBox(height: 10),
-        Text("Player "+ (_activePlayer + 1).toString() +" is playing.")
+        Text(_displayText)
       ],
     );
   }
@@ -94,6 +143,9 @@ class _TicTacToeState extends State<TicTacToe> {
   Widget build(BuildContext context) {
 
     _size = MediaQuery.of(context).size;
+    _displayText = "Player "+ (_activePlayer + 1).toString() +" is playing.";
+
+    _detectWins();
 
     return _buildGameField();
 
